2feab120e1252ae5ff5857550660645b596b068a
Ian Kirk <iankirk99@gmail.com>
2023-03-12 14:02:43 -0700

    cleaned up function logic after redirect

--- App.js
+++ App.js
@@ -30,27 +30,32 @@ export default function App() {
     return params;
   }
 
+  async function handleRedirect(url, platform) {
+    const params = parseQueryParams(url);
+    console.log("queryParams: ", params);
+    let codeRes;
+    codeRes = await fetch(
+      (platform === "ios"
+        ? "http://localhost:5001/codeResponse?"
+        : "http://10.0.2.2:5001/codeResponse?") +
+        new URLSearchParams({
+          code: params.code,
+          makeUserInfoReq: true,
+        })
+    );
+    const codeResponse = JSON.parse(await codeRes.json());
+    console.log("codeResponse: ", codeResponse);
+    setParsedJWT(codeResponse.idTokenParsed);
+    setUserInfo(codeResponse.userInfo);
+    setUserId(codeResponse.idTokenParsed.sub);
+  }
+
   useEffect(() => {
     if (Platform.OS === "android") {
       const func = async () => {
         Linking.addEventListener("url", async (event) => {
           console.log("event", event);
-          const params = parseQueryParams(event.url);
-          console.log("queryParams: ", params);
-          console.log("code: ", params.code);
-          let codeRes;
-          codeRes = await fetch(
-            "http://10.0.2.2:5001/codeResponse?" +
-              new URLSearchParams({
-                code: params.code,
-                makeUserInfoReq: true,
-              })
-          );
-          const codeResponse = JSON.parse(await codeRes.json());
-          console.log("codeResponse: ", codeResponse);
-          setParsedJWT(codeResponse.idTokenParsed);
-          setUserInfo(codeResponse.userInfo);
-          setUserId(codeResponse.idTokenParsed.sub);
+          await handleRedirect(event.url, "android");
         });
       };
       func();
@@ -63,48 +68,23 @@ export default function App() {
         <Button
           title="Log in (webViewController)"
           onPress={async () => {
-            console.log("about to request");
-            try {
-              // const res = await fetch("http://localhost:5001/codeResponse");
-              // const res = await fetch("https://auth.authillo.com/token");
-              // const res = await fetch("http://localhost:5001/getCodeChallenge");
-              let res;
-              if (Platform.OS === "android") {
-                res = await fetch("http://10.0.2.2:5001/getCodeChallenge");
-              } else {
-                res = await fetch("http://localhost:5001/getCodeChallenge");
-              }
-              const parsedResponse = JSON.parse(await res.json());
+            let res;
+            if (Platform.OS === "android") {
+              res = await fetch("http://10.0.2.2:5001/getCodeChallenge");
+            } else {
+              res = await fetch("http://localhost:5001/getCodeChallenge");
+            }
+            const parsedResponse = JSON.parse(await res.json());
 
-              const codeChallenge = parsedResponse.codeChallenge;
-              console.log("codeChallenge: ", codeChallenge);
-              const redirectLink = `http://ian.authillo.com/authorize?response_type=code&scope=${scopes}&state=undefined&redirect_uri=${redirectUrl}&client_id=${clientId}&max_age=${maxAge}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
-              let redirectRes = await WebBrowser.openAuthSessionAsync(
-                redirectLink
-              );
-              setHaveReturned(true);
-              console.log("redirectRes: ", redirectRes);
-              if (Platform.OS === "ios") {
-                const params = parseQueryParams(redirectRes.url);
-                console.log("queryParams: ", params);
-                console.log("code: ", params.code);
-                let codeRes;
-                codeRes = await fetch(
-                  "http://localhost:5001/codeResponse?" +
-                    new URLSearchParams({
-                      code: params.code,
-                      makeUserInfoReq: true,
-                    })
-                );
-
-                const codeResponse = JSON.parse(await codeRes.json());
-                console.log("codeResponse: ", codeResponse);
-                setParsedJWT(codeResponse.idTokenParsed);
-                setUserInfo(codeResponse.userInfo);
-                setUserId(codeResponse.idTokenParsed.sub);
-              }
-            } catch (err) {
-              console.log("err: ", err);
+            const codeChallenge = parsedResponse.codeChallenge;
+            console.log("codeChallenge: ", codeChallenge);
+            let redirectRes = await WebBrowser.openAuthSessionAsync(
+              `http://ian.authillo.com/authorize?response_type=code&scope=${scopes}&state=undefined&redirect_uri=${redirectUrl}&client_id=${clientId}&max_age=${maxAge}&code_challenge=${codeChallenge}&code_challenge_method=S256`
+            );
+            setHaveReturned(true);
+            console.log("redirectRes: ", redirectRes);
+            if (Platform.OS === "ios") {
+              await handleRedirect(redirectRes.url, "ios");
             }
           }}
         ></Button>
